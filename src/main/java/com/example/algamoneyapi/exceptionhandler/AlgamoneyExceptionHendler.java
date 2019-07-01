49afa77cf6c6a99e511bf1046c50a17f8f930046
Gabriel Salomão <strectrok@gmail.com>
2019-07-01 15:53:40 -0300

    Validando novos cadastros de categorias com nome sendo null ou com tamanhho invalido

--- src/main/java/com/example/algamoneyapi/exceptionhandler/AlgamoneyExceptionHendler.java
+++ src/main/java/com/example/algamoneyapi/exceptionhandler/AlgamoneyExceptionHendler.java
@@ -1,6 +1,11 @@
 package com.example.algamoneyapi.exceptionhandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+
+import javax.naming.Binding;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -11,6 +16,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -29,7 +37,29 @@ public class AlgamoneyExceptionHendler extends ResponseEntityExceptionHandler {
 	
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, Locale.US);
 		String mensagemDesenvolvedor = ex.getCause().toString();
-		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+		
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> criarListaDeErros(BindingResult bindingResult){
+		List<Erro> erros = new ArrayList<>();
+		
+		for(FieldError fieldError : bindingResult.getFieldErrors()) {
+			String mensagemUsuario = messageSource.getMessage(fieldError, Locale.US);
+			String mensagemDesenvolvedor = fieldError.toString();
+			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		}
+		
+		return erros;
 	}
 	
 	public static class Erro {
